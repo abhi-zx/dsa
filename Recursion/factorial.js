09664ccb91ac079eab2ad9a61ad7383e39876f60
Abhishek Singh <asingh3@icims.com>
2024-08-03 08:26:07 +0530

    questions

---
+++ Recursion/factorial.js
@@ -0,0 +1,34 @@
+
+
+function fact(input){
+    if(input<=1)
+    return input
+   let ans =  input * fact(input-1)
+   return ans
+}
+function runProgram(input) {
+ ans= fact(input)
+ console.log(ans)
+
+}
+
+if (process.env.USER === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
